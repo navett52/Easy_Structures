de74332ed2173fd8cddb504ff5a20adf7bed771d
Connor Tellep <jib11@live.com>
2016-06-24 02:03:34 -0400

    Writes attributes to file
    Now it writes attributes to the blank file created. I don't have access
    to the examples of the attributes available handy atm though, so I'll
    fix it for our particular use later.

--- src/configurationGenerator/YMLgenerator.java
+++ src/configurationGenerator/YMLgenerator.java
@@ -30,21 +30,60 @@ public class YMLgenerator
 	 * Method to create a yml file using a given file name(will use other attributes to write data to the file in future)
 	 * @param fileName = the given name of the file to be generated
 	 */
-	public void generateFile(String fileName)
+	public void generateFile(String fileName, String attribute1, String attribute2 ,String attribute3 ,String attribute4 ,String attribute5)
 	{
 		this.fileName = fileName;
-		fileName = fileName + ".yml";
-		//Creates a new file object with the file name
-		File file = new File(fileName);
+		BufferedWriter writer = null;
+		
+		this.attribute1 = attribute1;
+		this.attribute2 = attribute2;
+		this.attribute3 = attribute3;
+		this.attribute4 = attribute4;
+		this.attribute5 = attribute5;
+		
 		
 		try 
 		{
+			fileName = fileName + ".yml";
+			//Creates a new file object with the file name
+			File file = new File(fileName);
+			
+			String filePath = file.getCanonicalPath();
+			
 			//creates a blank file from the file object with the given file name
 			file.createNewFile();
-		} catch (IOException e) 
+			
+			
+			//writes attributes to the file
+			writer = new BufferedWriter(new FileWriter(filePath));
+			writer.write("Biome: " + attribute1);
+			writer.newLine();
+			writer.write("Spawn Chance: " + attribute2);
+			writer.newLine();
+			writer.write("Basement Depth: " + attribute3);
+			writer.newLine();
+			writer.write("Biome: " + attribute4);
+			writer.newLine();
+			writer.write("Biome: " + attribute5);
+			
+			
+			
+		} 
+		catch (IOException e) 
 		{
 			System.err.println(e.getLocalizedMessage());
 			e.printStackTrace();
+		}
+		finally
+		{
+			try 
+			{
+				writer.close();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 }

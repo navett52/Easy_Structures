8b777cd4f0f444f46f3980f5866aa5897ebbdf02
Evan Tellep <evantellep@yahoo.com>
2016-06-24 15:48:01 -0400

    Added Comments
    Added comments to the configuration generator gui code

--- src/configurationGenerator/Main.java
+++ src/configurationGenerator/Main.java
@@ -6,15 +6,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
+	//Linking properties to text fields in the gui
 	@FXML
 	private TextField fileName;
 	@FXML
@@ -38,14 +36,15 @@ public class Main extends Application
 	
 	public static void main(String[] args) 
 	{	
+		//Launches the gui
 		launch(args);
-		//YMLgenerator yml = new YMLgenerator();
-		//yml.generateFile("d", "d", "d", "d", "d", "d", "d", "d", "d");
 	}
-	
+	//The on click event that takes all the attributes and generates the config file
 	@FXML
 	public void OnClick(ActionEvent evt) {
+		//Instantiating the YMLgenerator
 		YMLgenerator yml = new YMLgenerator();
+		//Grabbing the attribute values from the text fields of the gui
 		String nameOfFile = fileName.getText();
 		String placeToSpawn = place.getText();
 		String maxNumOfSpawns = maxSpawns.getText();
@@ -55,11 +54,13 @@ public class Main extends Application
 		String maximumHeight = maxHeight.getText();
 		String randomRotate = randomRotation.getText();
 		String pasteSchematicAir = pasteAir.getText();
+		//Generating the config file using the valuse grabbed from the gui
 		yml.generateFile(nameOfFile, placeToSpawn, maxNumOfSpawns, spawnChance, depthOfbasement, minimumHeight, maximumHeight, randomRotate, pasteSchematicAir);
 	}
-	
+	//Building the gui
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//Instantiating all the interactive pieces of the gui
 		fileName = new TextField();
 		place = new TextField();
 		maxSpawns = new TextField();
@@ -70,15 +71,25 @@ public class Main extends Application
 		randomRotation = new TextField();
 		pasteAir = new TextField();
 		create = new Button();
+		//Trying to build the gui
 		try {
-			Pane root;
+			//Getting the pane used to hold all the parts of the gui
+			BorderPane root;
+			//Instantiating a loader to load my FXML file
 			FXMLLoader loader = new FXMLLoader();
+			//Telling the loader which file to load and where
 			loader.setLocation(Main.class.getResource("/configurationGenerator/ConfigurationGeneratorScene.fxml"));
+			//I have an event so I need to tell the FXML file this is the controller
 			loader.setController(this);
-			root = (Pane) loader.load();
+			//Loading the pane from the FXML file
+			root = (BorderPane) loader.load();
+			//Creating the scene to hold the pane and everything else
 			Scene scene = new Scene(root, 600, 360);
+			//Setting the stage to not be resizable
 			primaryStage.setResizable(false);
+			//Setting the scene to be in the stage
 			primaryStage.setScene(scene);
+			//Show the stage
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();

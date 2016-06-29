f93941a9ada10c8c5e626c51f97a09d31e591d52
Connor Tellep <jib11@live.com>
2016-06-28 23:58:59 -0400

    Edited for better practice and better functionality
    Added the variables to the constructor of the YML object instead of the
    generate file method. Edited the main appropriately. Added comments and
    slightly better exception handling

--- src/configurationGenerator/Main.java
+++ src/configurationGenerator/Main.java
@@ -41,9 +41,9 @@ public class Main extends Application
 	}
 	//The on click event that takes all the attributes and generates the config file
 	@FXML
-	public void OnClick(ActionEvent evt) {
-		//Instantiating the YMLgenerator
-		YMLgenerator yml = new YMLgenerator();
+	public void OnClick(ActionEvent evt) 
+	{
+		
 		//Grabbing the attribute values from the text fields of the gui
 		String nameOfFile = fileName.getText();
 		String placeToSpawn = place.getText();
@@ -54,12 +54,18 @@ public class Main extends Application
 		String maximumHeight = maxHeight.getText();
 		String randomRotate = randomRotation.getText();
 		String pasteSchematicAir = pasteAir.getText();
+		
+		//Instantiating the YMLgenerator
+		YMLgenerator yml = new YMLgenerator(nameOfFile, placeToSpawn, maxNumOfSpawns, spawnChance, depthOfbasement, minimumHeight, maximumHeight, randomRotate, pasteSchematicAir);
+		
 		//Generating the config file using the valuse grabbed from the gui
-		yml.generateFile(nameOfFile, placeToSpawn, maxNumOfSpawns, spawnChance, depthOfbasement, minimumHeight, maximumHeight, randomRotate, pasteSchematicAir);
+		yml.generateFile();
 	}
+	
 	//Building the gui
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception 
+	{
 		//Instantiating all the interactive pieces of the gui
 		fileName = new TextField();
 		place = new TextField();
@@ -71,27 +77,40 @@ public class Main extends Application
 		randomRotation = new TextField();
 		pasteAir = new TextField();
 		create = new Button();
+		
 		//Trying to build the gui
 		try {
 			//Getting the pane used to hold all the parts of the gui
 			BorderPane root;
+			
 			//Instantiating a loader to load my FXML file
 			FXMLLoader loader = new FXMLLoader();
+			
 			//Telling the loader which file to load and where
 			loader.setLocation(Main.class.getResource("/configurationGenerator/ConfigurationGeneratorScene.fxml"));
+			
 			//I have an event so I need to tell the FXML file this is the controller
 			loader.setController(this);
+			
 			//Loading the pane from the FXML file
 			root = (BorderPane) loader.load();
+			
 			//Creating the scene to hold the pane and everything else
 			Scene scene = new Scene(root, 600, 360);
+			
 			//Setting the stage to not be resizable
 			primaryStage.setResizable(false);
+			
 			//Setting the scene to be in the stage
 			primaryStage.setScene(scene);
+			
 			//Show the stage
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
+			System.err.println(e.getLocalizedMessage());
+			System.err.println("GUI COULDN'T LOAD");
 			e.printStackTrace();
 		}
 	}
--- src/configurationGenerator/YMLgenerator.java
+++ src/configurationGenerator/YMLgenerator.java
@@ -22,22 +22,24 @@ public class YMLgenerator
 	private String maxHeight;
 	private String randomRotation;
 	private String pasteAir;
-	//no args constructor for object construction, will be removed later
-	public YMLgenerator()
-	{
-		
-	}
+	
 	
 	/**
-	 * Method to create a yml file using a given file name(will use other attributes to write data to the file in future)
-	 * @param fileName = the given name of the file to be generated
+	 * Constructor for YMLgenerator object
+	 * @param fileName = Name of the file to be generated
+	 * @param place = place the schematic will be pasted
+	 * @param maxSpawns = maximum spawns of a schematic
+	 * @param chanceToSpawn = variable that determines the percent chance of a schematic randomly spawning
+	 * @param basementDepth = the offset of a schematic to the blocks it's pasted on
+	 * @param minHeight = the lowest height that a schematic can be spawned
+	 * @param maxHeight = the highest height that a schematic can be spawned
+	 * @param randomRotation = determines whether or not you want a schematic randomly rotated when spawning
+	 * @param pasteAir = determines whether or not you want a schematic pasted with air blocks included
 	 */
-	public void generateFile(String fileName, String place, String maxSpawns ,String chanceToSpawn ,String basementDepth ,String minHeight, 
-			String maxHeight, String randomRotation, String pasteAir)
+	public YMLgenerator(String fileName, String place, String maxSpawns ,String chanceToSpawn ,String basementDepth ,String minHeight, String maxHeight, String randomRotation, String pasteAir)
 	{
+		//Sets the class level variables to equal the construction values
 		this.fileName = fileName;
-		BufferedWriter writer = null;
-		
 		this.place = place;
 		this.maxSpawns = maxSpawns;
 		this.chanceToSpawn = chanceToSpawn;
@@ -46,32 +48,75 @@ public class YMLgenerator
 		this.maxHeight = maxHeight;
 		this.randomRotation = randomRotation;
 		this.pasteAir = pasteAir;
+	}
+	
+	/**
+	 * Method to generate a file using the current YMLgenerator object
+	 */
+	public void generateFile()
+	{
+		//creates a null buffered writer object
+		BufferedWriter writer = null;
 		
+		
+		//tries to create and write a new YML file
 		try 
 		{
+			//adds the ".yml" extension to the file name
 			fileName = fileName + ".yml";
+			
 			//Creates a new file object with the file name
 			File file = new File(fileName);
-			String filePath = file.getCanonicalPath();
+			
 			//creates a blank file from the file object with the given file name
 			file.createNewFile();
-			//writes attributes to the file
+			
+			//finds the exact path of the created file object to ensure writing to it is successful 
+			String filePath = file.getCanonicalPath();
+			
+			//Instantiate the buffered writer object wrapped around a file writer object
 			writer = new BufferedWriter(new FileWriter(filePath));
-			writer.write("place: " + place);
-			writer.newLine();
-			writer.write("maxspawns: " + maxSpawns);
-			writer.newLine();
-			writer.write("chance: " + chanceToSpawn);
-			writer.newLine();
-			writer.write("basementdepth: " + basementDepth);
-			writer.newLine();
-			writer.write("anywhereminY: " + minHeight);
-			writer.newLine();
-			writer.write("anywheremaxY: " + maxHeight);
-			writer.newLine();
-			writer.write("randomrotate: " + randomRotation);
-			writer.newLine();
-			writer.write("pasteschematicair: " + pasteAir);
+			
+			//Writes individual attributes to the file with a line break at the end of each attribute IF they actually entered said attributes
+			if(place != null)
+			{
+				writer.write("place: " + place);
+				writer.newLine();
+			}
+			if(maxSpawns != null)
+			{
+				writer.write("maxspawns: " + maxSpawns);
+				writer.newLine();
+			}
+			if(chanceToSpawn != null)
+			{
+				writer.write("chance: " + chanceToSpawn);
+				writer.newLine();
+			}
+			if(basementDepth != null)
+			{
+				writer.write("basementdepth: " + basementDepth);
+				writer.newLine();
+			}
+			if(minHeight != null)
+			{
+				writer.write("anywhereminY: " + minHeight);
+				writer.newLine();
+			}
+			if(maxHeight != null)
+			{
+				writer.write("anywheremaxY: " + maxHeight);
+				writer.newLine();
+			}
+			if(randomRotation != null)
+			{
+				writer.write("randomrotate: " + randomRotation);
+				writer.newLine();
+			}
+			if(pasteAir != null)
+			{
+				writer.write("pasteschematicair: " + pasteAir);
+			}
 		} 
 		catch (IOException e) 
 		{
@@ -86,6 +131,7 @@ public class YMLgenerator
 			} 
 			catch (IOException e) 
 			{
+				System.err.println(e.getLocalizedMessage());
 				e.printStackTrace();
 			}
 		}
